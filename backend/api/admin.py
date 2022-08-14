02b854f39ec737a7bdd27e08d491ceb2aa61fa1c
Alexander Shelpyakov <ashelpyakov@gmail.com>
2022-08-14 12:04:12 +0500

    FIX search in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin import display
+from django.utils.translation import gettext_lazy as _
 
 from .models import (
     Ingredient, Favorite, Recipe,
@@ -9,40 +11,45 @@ from .models import (
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'measurement_unit')
-    list_filter = ('name',)
     search_fields = ('name',)
 
 
 @admin.register(Tag)
-class IngredientAdmin(admin.ModelAdmin):
+class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'color', 'slug')
-    list_filter = ('name',)
     search_fields = ('name',)
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'author')
-    list_filter = ('author', 'name', 'tags')
-    search_fields = ('author', 'name', 'tags')
+    list_filter = ('author', 'tags')
+    search_fields = ('author__email', 'name', 'tags__name')
 
 
 @admin.register(RecipeIngredients)
 class RecipeIngredientsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ingredient', 'recipe', 'amount')
+    list_display = (
+        'id', 'ingredient', 'recipe',
+        'get_measurement_unit', 'amount',
+    )
     list_filter = ('ingredient',)
-    search_fields = ('ingredient', 'recipe')
+    search_fields = ('ingredient__name', 'recipe__name')
+
+    @display(description=_('measurement unit'))
+    def get_measurement_unit(self, obj):
+        return obj.ingredient.measurement_unit
 
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'recipe')
     list_filter = ('user', 'recipe')
-    search_fields = ('user', 'recipe')
+    search_fields = ('user__email', 'recipe__name')
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'recipe')
     list_filter = ('user', 'recipe')
-    search_fields = ('user', 'recipe')
+    search_fields = ('user__email', 'recipe__name')

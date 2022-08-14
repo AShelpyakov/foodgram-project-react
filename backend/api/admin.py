2931d1b72c2c19f865560d8939dc9fcd106be585
Alexander Shelpyakov <ashelpyakov@gmail.com>
2022-08-14 10:38:10 +0500

    ADD admin for api models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Ingredient, Tag
+from .models import (
+    Ingredient, Favorite, Recipe,
+    RecipeIngredients, Tag, ShoppingCart,
+)
 
 
 @admin.register(Ingredient)
@@ -15,3 +18,31 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'color', 'slug')
     list_filter = ('name',)
     search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author')
+    list_filter = ('author', 'name', 'tags')
+    search_fields = ('author', 'name', 'tags')
+
+
+@admin.register(RecipeIngredients)
+class RecipeIngredientsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ingredient', 'recipe', 'amount')
+    list_filter = ('ingredient',)
+    search_fields = ('ingredient', 'recipe')
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user', 'recipe')
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user', 'recipe')

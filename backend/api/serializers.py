2a4b36107f21a3df84073241c791887bcfd39c16
Alexander Shelpyakov <ashelpyakov@gmail.com>
2022-08-15 14:19:35 +0500

    ADD View for Favorite

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
 
-from .models import Ingredient, Tag
+from .models import (
+    Ingredient, Favorite, Recipe,
+    RecipeIngredients, Tag, ShoppingCart,
+)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
@@ -13,3 +17,48 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name', 'color', 'slug')
+
+
+class RecipeIngredientsSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
+    class Meta:
+        model = RecipeIngredients
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+
+class ShortRecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image', 'cooking_time')
+        read_only_fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ('user', 'recipe')
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        return ShortRecipeSerializer(
+            instance.recipe, context={'request': request}
+        ).data
+
+    def validate(self, data):
+        user = data['user']
+        recipe = data['recipe']
+        if Favorite.objects.filter(user=user, recipe=recipe).exists():
+            raise serializers.ValidationError(
+                {'status': _('the recipe is already in favorites')}
+            )
+        return data
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingCart
+        fields = ('user', 'recipe')
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,21 @@
 from rest_framework import filters, viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from django.utils.translation import gettext_lazy as _
 
-from .models import Ingredient, Tag
-from .serializers import IngredientSerializer, TagSerializer
+from .models import Ingredient, Recipe, Tag, Favorite
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (
+    IngredientSerializer, TagSerializer, FavoriteSerializer
+)
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
@@ -17,3 +30,39 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (AllowAny,)
+
+
+class RecipeViewSet(ModelViewSet):
+    queryset = Recipe.objects.all()
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    @action(
+        detail=True,
+        permission_classes=(IsAuthenticated,),
+        methods=('post',)
+    )
+    def favorite(self, request, pk):
+        data = {
+            'user': request.user.id,
+            'recipe': pk,
+        }
+        serializer = FavoriteSerializer(
+            data=data,
+            context={'request': request},
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    @favorite.mapping.delete
+    def delete_favorite(self, request, pk):
+        user = request.user
+        try:
+            favorite = Favorite.objects.get(user=user, recipe=pk)
+        except Favorite.DoesNotExist:
+            return Response(
+                {'error': _('the recipe is not in favorites')},
+                status=HTTP_400_BAD_REQUEST,
+            )
+        favorite.delete()
+        return Response(status=HTTP_204_NO_CONTENT)

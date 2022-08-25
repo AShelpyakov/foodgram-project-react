4a8efc0888077c794da7ea028aa6af71f448c77c
Alexander Shelpyakov <ashelpyakov@gmail.com>
2022-08-25 21:40:42 +0500

    FIX Admin for User

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Follow, User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email')
     list_filter = ('username', 'email')
